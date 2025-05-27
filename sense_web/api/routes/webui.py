9b3fd34ab93aad77b50079cccdc612d43bdbe757
skwort <skwort@skwort.dev>
2025-05-27 18:07:57 +1000

    api: Render device datapoints in WebUI
    Update the datapoint table to correctly render datapoints for the
    device.

--- sense_web/api/routes/webui.py
+++ sense_web/api/routes/webui.py
@@ -2,6 +2,7 @@ import uuid
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sense_web.services.datapoint import get_datapoints_by_device_uuid
 from sense_web.services.device import list_devices, get_device_by_uuid
 from sense_web.services.ipc import peek_commands
 
@@ -26,7 +27,14 @@ async def device(uuid: uuid.UUID, request: Request) -> HTMLResponse:
 
     commands = await peek_commands(str(uuid))
 
+    datapoints = await get_datapoints_by_device_uuid(uuid)
+
     return templates.TemplateResponse(
         "device.html",
-        {"request": request, "device": device, "commands": commands},
+        {
+            "request": request,
+            "device": device,
+            "commands": commands,
+            "datapoints": datapoints,
+        },
     )
